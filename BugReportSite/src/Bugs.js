5eafe2e9bb09935eacbb8b5bcadd598fff2e08bf
Kræn Byskov <kb@parkpark.dk>
2021-06-22 21:59:32 +0200

    update af bugreport side

--- BugReportSite/src/Bugs.js
+++ BugReportSite/src/Bugs.js
@@ -1,15 +1,11 @@
-import React, { useState } from "react"
+import React, { useState } from 'react';
 import firebase from 'firebase';
-
+import { Table, Navbar, Nav, Button, Badge } from 'react-bootstrap';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-
 
 function Bugs() {
     const [GetData, setGetData] = useState([]);
-    const query = firebase
-        .firestore()
-        .collection('Bugs')
-        .orderBy('Date', "desc");
+    const query = firebase.firestore().collection('Bugs').orderBy('Date', 'desc');
     const [Food] = useCollectionData(query);
 
     React.useEffect(
@@ -19,23 +15,52 @@ function Bugs() {
         [Food]
     );
 
-
-
     return (
         <div className="App">
-            <h1>Bugs</h1>
-            <ul>
-                {GetData && GetData.map((bugs) => (
-                    <li>
-                        <h2>Title : {bugs.Title}</h2>
-                        <h4>Owner : {bugs.Owner}</h4>
-                        <p>Description : {bugs.Description}</p>
-                        <img style={{ width: 200 }} src={bugs.downloadUrl} />
-                    </li>
-                ))}
-            </ul>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Navbar>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>Bugs</th>
+                        <th>Title</th>
+                        <th>Owner</th>
+                        <th>Error Code</th>
+                        <th>Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {GetData &&
+                        GetData.map((bugs, index) => (
+                            <tr>
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    {bugs.Title}
+                                </td>
+                                <td>
+                                    {bugs.Owner}
+                                </td>
+                                <td style={{ textAlign: "center" }}>
+                                    <h5><Badge size="lg" variant="primary">New</Badge></h5>
+                                </td>
+                                <td>
+                                    <Button size="sm" variant="primary">Link</Button>
+                                </td>
+
+                            </tr>
+                        ))}
+                </tbody>
+            </Table>
         </div>
-    )
+    );
 }
 
-export default Bugs
+export default Bugs;
